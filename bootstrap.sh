e169bb1841f07f3f0f39e68c7c57bc241db6b66a
Quang Vu <vhquang@gmail.com>
2019-07-13 12:35:32 -0700

    Add ZSH install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,26 +2,47 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+# git pull origin master;
 
-function doIt() {
+function setupZshUbuntu() {
+	# install ZSH if needed
+	if [ -z $(which zsh) ]; then
+		echo "Installing zsh";
+		sudo apt install -y zsh;
+		sudo chsh -s $(which zsh);
+	fi
+
+	# setup Oh-my-ZSH
+	if [ ! -d ~/.oh-my-zsh ]; then
+		echo "Installing oh-my-zsh";
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	fi
+}
+
+function syncSettings() {
 	rsync --exclude ".git/" \
+		--exclude ".gitconfig" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
+		--exclude "symlink-setup.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
+setupZshUbuntu;
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	syncSettings;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		syncSettings;
 	fi;
 fi;
-unset doIt;
+
+unset setupZsh;
+unset syncSettings;
