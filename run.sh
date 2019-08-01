ea9ced40a31a4c841cc4f8859e40090f5fae7aa2
Quang Vu <vhquang@gmail.com>
2019-08-01 08:35:17 -0700

    Update install python script

--- run.sh
+++ run.sh
@@ -1,34 +1,42 @@
 function install_python() {
-    # TODO check conda not base
     if [[ "$(which python)" != *"conda"* || "$(which python)" != *"envs"* ]]; then
         echo "Not in non-base conda environment"
+
     else
     # TODO terminal color
-    _TEMP_PY_SRC="/tmp/temp_install_python"
-    echo "setting up $_TEMP_PY_SRC" && echo ""
-    # rm -rf $_TEMP_PY_SRC && mkdir -p $_TEMP_PY_SRC && cd $_TEMP_PY_SRC
 
-    # _PY_URL=$1
-    # _py_tgz_file=$(basename $_PY_URL)
-    # unpack_dir="${_py_tgz_file%.*}"
+        _TEMP_PY_SRC="/tmp/temp_install_python"
+        echo "Setting up $_TEMP_PY_SRC" && echo ""
+        rm -rf $_TEMP_PY_SRC && mkdir -p $_TEMP_PY_SRC && cd $_TEMP_PY_SRC
 
-    # wget $_PY_URL
-    # tar xf $_py_tgz_file && cd $unpack_dir
+        _PY_URL=$1
+        _py_tgz_file=$(basename $_PY_URL)
+        unpack_dir="${_py_tgz_file%.*}"
 
-    # bin_dir=$(dirname $(which python))
-    # install_dir="${bin_dir%/bin}"
+        wget $_PY_URL
+        tar xf $_py_tgz_file && cd $unpack_dir
 
-    # echo "replacing $bin_dir/python3"
-    # rm -f "$bin_dir"/python3 "$bin_dr"/python
-    # ./configure --prefix=$install_dir && \
-    #         make clean && \
-    #         make -j 8 && make install
+        unset _PY_URL
+        unset _TEMP_PY_SRC
+        unset _py_tgz_file
+        unset unpack_dir
 
-    # # TODO unset all vars
-    # unset _PY_URL
-    # unset _TEMP_PY_SRC
-    # unset _py_tgz_file
-    # unset unpack_dir
+
+        _py_bin_dir=$(dirname $(which python))
+        install_dir="${_py_bin_dir%/bin}"
+
+        echo "Replacing $_py_bin_dir/python3"
+        rm -f "$_py_bin_dir/python3"
+        # TODO need to manually create `python` link
+        # rm -f "$_py_bin_dir/python"
+
+        make clean && \
+                ./configure --prefix=$install_dir && \
+                make -j 8 && make install
+
+        unset _py_bin_dir
+        unset install_dir
+
     fi
 }
 
