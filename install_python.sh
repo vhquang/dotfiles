d2b548224ed18e40fc56d73e521e2e38a76d2f93
Quang Vu <vhquang@gmail.com>
2019-08-01 09:19:09 -0700

    Manually create new python symlink

--- install_python.sh
+++ install_python.sh
@@ -11,7 +11,7 @@ function install_python() {
 
         _PY_URL=$1
         _py_tgz_file=$(basename $_PY_URL)
-        unpack_dir="${_py_tgz_file%.*}"
+        unpack_dir="$_TEMP_PY_SRC/${_py_tgz_file%.*}"
 
         wget $_PY_URL
         tar xf $_py_tgz_file && cd $unpack_dir
@@ -19,30 +19,36 @@ function install_python() {
         unset _PY_URL
         unset _TEMP_PY_SRC
         unset _py_tgz_file
-        unset unpack_dir
 
 
         _py_bin_dir=$(dirname $(which python))
         install_dir="${_py_bin_dir%/bin}"
 
-        echo "Replacing $_py_bin_dir/python3"
-        rm -f "$_py_bin_dir/python3"
-        # TODO need to manually create `python` link
-        # rm -f "$_py_bin_dir/python"
-
-        make clean && \
-                ./configure --prefix=$install_dir && \
+        make clean
+        ./configure --prefix=$install_dir && \
                 make -j 8 && make install
 
+        # set new python symlink
+        echo "Replacing $_py_bin_dir/python3"
+        _py_version=$($unpack_dir/python --version)
+        _py_version_short=${_py_version:7:3}
+        cd $_py_bin_dir
+        rm -f "python3" "python"
+        ln -s "python$_py_version_short" "python3"
+        ln -s "python$_py_version_short" "python"
+
+        unset unpack_dir
         unset _py_bin_dir
         unset install_dir
+        unset _py_version
+        unset _py_version_short
 
     fi
 }
 
 
 function setup_conda() {
-    conda remove --all -n py38b3 -y
+    conda deactivate && conda remove --all -n py38b3 -y
     conda create -n py38b3 -y python=3 && conda activate py38b3
 }
 
